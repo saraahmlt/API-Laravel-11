b7a8a7a82902ec2d640c75474143fc583b242c63
Sarah Mourlevat <sarah.mourlevat@gmail.com>
2024-05-07 11:47:25 +0200

    swagger

--- database/migrations/2024_05_02_095830_create_users_table.php
+++ database/migrations/2024_05_02_095830_create_users_table.php
@@ -20,8 +20,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('two_factor_secret')->nullable(); // Ajout de la colonne two_factor_secret
-            $table->text('two_factor_recovery_codes')->nullable(); // Ajout de la colonne two_factor_recovery_codes
+            $table->text('two_factor_secret')->nullable(); 
+            $table->text('two_factor_recovery_codes')->nullable(); 
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
